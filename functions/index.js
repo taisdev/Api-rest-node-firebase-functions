3e5082b37decb73afeafc828ca9aa209f4483faa
Taís Oliveira <tsoliveira868@gmail.com>
2022-04-09 15:42:46 -0300

    Atualização das funções

--- functions/index.js
+++ functions/index.js
@@ -23,50 +23,76 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/:email", async (req, res) => {
-  const snapshot = await admin.firestore().collection("users")
-      .where("email", "==", req.params.email).get();
+  try {
+    const snapshot = await admin.firestore().collection("users")
+        .where("email", "==", req.params.email).get();
 
-  const users = [];
-  snapshot.forEach((doc) => {
-    const id = doc.id;
-    const data = doc.data();
+    const users = [];
+    snapshot.forEach((doc) => {
+      const id = doc.id;
+      const data = doc.data();
 
-    users.push({id, ...data});
-  });
-
-  res.status(200).send(JSON.stringify(users));
+      users.push({id, ...data});
+    });
+    if (users.length > 0) {
+      res.status(200).send(JSON.stringify(users));
+    } else {
+      res.send("Nenhum usuário encontrado");
+    }
+  } catch (error) {
+    res.status(500).send("Nenhum usuário encontrado" + error.message);
+  }
 });
 
 app.get("/:id", async (req, res) => {
-  const snapshot = await admin.firestore()
-      .collection("users").doc(req.params.id).get();
+  try {
+    const snapshot = await admin.firestore()
+        .collection("users").doc(req.params.id).get();
 
-  const userId = snapshot.id;
-  const userData = snapshot.data();
+    const userId = snapshot.id;
+    const userData = snapshot.data();
 
-  res.status(200).send(JSON.stringify({id: userId, ...userData}));
+    res.status(200).send(JSON.stringify({id: userId, ...userData}));
+  } catch (error) {
+    res.status(500).send("Nenhum usuário encontrado" + error.message);
+  }
 });
 
 app.post("/", async (req, res) => {
-  const user = req.body;
+  const {usuario, email, tipo} = req.body;
+  const user = {
+    usuario,
+    email,
+    tipo,
+  };
+  try {
+    await admin.firestore().collection("users").add(user);
 
-  await admin.firestore().collection("users").add(user);
-
-  res.status(201).send();
+    res.status(201).send("Usuário cadastrado com sucesso!");
+  } catch (error) {
+    res.send("Erro ao cadastrar usuário: " + error.message);
+  }
 });
 
 app.put("/:id", async (req, res) => {
   const body = req.body;
+  try {
+    await admin.firestore().collection("users").doc(req.params.id).update(body);
 
-  await admin.firestore().collection("users").doc(req.params.id).update(body);
-
-  res.status(200).send();
+    res.status(200).send("Usuário atualizado com sucesso!");
+  } catch (error) {
+    res.send("Erro ao atualizar usuário: " + error.message);
+  }
 });
 
 app.delete("/:id", async (req, res) => {
-  await admin.firestore().collection("users").doc(req.params.id).delete();
+  try {
+    await admin.firestore().collection("users").doc(req.params.id).delete();
 
-  res.status(200).send();
+    res.status(200).send("Usuário deletado com sucesso!");
+  } catch (error) {
+    res.send("Erro ao atualizar usuário: " + error.message);
+  }
 });
 
 exports.user = functions.https.onRequest(app);
